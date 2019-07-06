81a38efbbc3563c6410a3ba33969410c29494ea1
Suhas <suhaskn09@gmail.com>
2019-07-06 17:44:02 +0200

    game logic

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,45 @@
-// Write your JS here
+const hero = {
+    name: 'Batman',
+    heroic: true,
+    health: 10,
+    inventory: [],
+    weapon: {
+        type: 'Sword',
+        damage: 2
+    }  
+}
+
+function rest(hero) {
+
+    if(hero.health === 10)
+    {
+        alert('Health is already 10');
+    }
+    else
+    {
+        hero.health = 10;
+    }
+
+    return hero;  
+}
+
+function pickUpItem(hero, weaponLike) {
+    
+    hero.inventory.push(weaponLike);
+
+}
+
+function equipWeapon(hero) {
+
+    if(hero.inventory.length)
+    {
+        hero.weapon = hero.inventory[0];
+    }
+  
+}
+
+function resetHealth() {
+    
+    hero.health = 10;
+
+}
